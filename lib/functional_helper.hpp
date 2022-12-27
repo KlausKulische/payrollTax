db0ae2758decd3b567c638cf993a800341a3e2eb
KlausKulische <klaus.kulischke@web.de>
2022-12-27 23:24:07 +0100

    Removed unused functions

--- lib/functional_helper.hpp
+++ lib/functional_helper.hpp
@@ -7,10 +7,8 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
-//static constexpr auto input;
-extern std::vector<std::string> stringVec;
-extern const std::string vecOutput(std::vector<std::string> &inputVec);
-const std::string loadConfig();
+
+
 int num_children(int num);
 std::string children_string (int num);
 std::string children_num (int num);
